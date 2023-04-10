d7591a9081f921b38dcd0c6ee82064f1c2e0f395
arifrahmadi23 <muhammad231arif@gmail.com>
2023-04-10 15:13:08 +0700

    memperbaiki sesuai contoh

--- class_oop_171/class_oop_171.cpp
+++ class_oop_171/class_oop_171.cpp
@@ -29,8 +29,8 @@ public:
 
 	void tampilMK() {
 		cout << "\nJumlah sks = " << sks;
-		cout << "Kode MK = " << kode;
-		cout << "Nama MK = " << namaMk << endl;
+		cout << "\nKode MK = " << kode;
+		cout << "\nNama MK = " << namaMk << endl;
 
 	}
 };
